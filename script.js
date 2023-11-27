5fd0c741f1362f51ee07996ae4e59a698e0a96e0
Maksym <makkie.makowski@gmail.com>
2023-11-27 18:19:47 -0500

    add pre in postorder methods

--- script.js
+++ script.js
@@ -14,7 +14,77 @@ class BinarySearchTree {
     isEmpty() {
         return this.root === null
     }
+
+    insert(value) {
+        const newNode = new Node(value)
+        if(this.isEmpty()) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        if(newNode.value < root.value) {
+            if(root.left === null) {
+                root.left = newNode
+            } else {
+               this.insertNode(root.left, newNode)
+            }
+        } else {
+            if(root.right === null) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
+    search(root, value) {
+        if(!root) {
+            return false
+        } else {
+            if(root.value === value) {
+                return true
+            } else if (value < root.value) {
+                return this.search(root.left, value)
+            } else {
+                return this.search(root.right, value)
+            }
+        }
+    }
+
+    preOrder(root) {
+        if(root) {
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+
+    inOrder(root) {
+        if(root) {
+            this.inOrder(root.left)
+            console.log(root.value)
+            this.inOrder(root.right) 
+        }
+    }
+
+    postOrder(root) {
+        if(root) {
+            this.inOrder(root.left)
+            this.inOrder(root.right) 
+            console.log(root.value)
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
 console.log('the tree is empty: ', bst.isEmpty());
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+
+bst.postOrder(bst.root);
