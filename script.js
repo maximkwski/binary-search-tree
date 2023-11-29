a207cc7f69807de4d3a5a003fc86116b690796c1
Maksym <makkie.makowski@gmail.com>
2023-11-29 13:47:31 -0500

    add delete node method

--- script.js
+++ script.js
@@ -77,6 +77,65 @@ class BinarySearchTree {
             console.log(root.value)
         }
     }
+
+    levelOrder() {
+        const queue = []
+        queue.push(this.root) 
+        while(queue.length) {
+            let curr = queue.shift()
+            console.log(curr.value)
+            if(curr.left) {
+                queue.push(curr.left)
+            }
+            if(curr.right) {
+                queue.push(curr.right)
+            }
+        }
+    }
+
+    min(root) {
+        if(!root.left) {
+            return root.value
+        } else {
+            return this.min(root.left)
+        }
+    }
+
+    max(root) {
+        if(!root.right) {
+            return root.value
+        } else {
+            return this.max(root.right)
+        }
+    }
+
+    delete(value) {
+        this.root = this.deleteNode(this.root, value)
+    }
+
+    deleteNode(root, value) {
+        if(root === null) {
+            return root
+        }
+        if(value < root.value) {
+            root.left = this.deleteNode(root.left, value)
+        } else if(value > root.value) {
+            root.right = this.deleteNode(root.right, value)
+        } else {
+            if(!root.left && !root.right) {
+                return null
+            }
+            if(!root.left) {
+                return root.right
+            } else if(!root.right) {
+                return root.left
+            }
+            root.value = this.min(root.right)
+            root.right = this.deleteNode(root.right, root.value)
+            
+        }
+        return root
+    }
 }
 
 const bst = new BinarySearchTree();
@@ -85,6 +144,5 @@ bst.insert(10)
 bst.insert(5)
 bst.insert(15)
 bst.insert(3)
-bst.insert(7)
 
-bst.postOrder(bst.root);
+bst.levelOrder()
